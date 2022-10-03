ba079f07935d1e962c3c269b83b8f0203882e169
Roman Hutsoliak <roman.hutsoliak@gmail.com>
2022-10-03 09:28:11 +0300

    Icons optimizations

--- src/components/pages/Tasks.tsx
+++ src/components/pages/Tasks.tsx
@@ -193,7 +193,18 @@ export default function Tasks() {
                                                     )
                                                 }
                                             >
-                                                <i className="bi bi-eye"></i>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="16"
+                                                    height="16"
+                                                    fill="currentColor"
+                                                    className="bi bi-eye"
+                                                    viewBox="0 0 16 16"
+                                                    focusable="false"
+                                                >
+                                                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                                                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                                                </svg>
                                             </a>
                                             <button
                                                 className="btn btn-link"
@@ -205,7 +216,18 @@ export default function Tasks() {
                                                     completeTaskId.current = id;
                                                 }}
                                             >
-                                                <i className="bi bi-check-circle"></i>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="16"
+                                                    height="16"
+                                                    fill="currentColor"
+                                                    className="bi bi-check-circle"
+                                                    viewBox="0 0 16 16"
+                                                    focusable="false"
+                                                >
+                                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                                                </svg>
                                             </button>
                                             <a
                                                 className="btn btn-link"
@@ -220,7 +242,17 @@ export default function Tasks() {
                                                     );
                                                 }}
                                             >
-                                                <i className="bi bi-pencil-fill"></i>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="16"
+                                                    height="16"
+                                                    fill="currentColor"
+                                                    className="bi bi-pencil-fill"
+                                                    viewBox="0 0 16 16"
+                                                    focusable="false"
+                                                >
+                                                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                                                </svg>
                                             </a>
                                         </td>
                                     </tr>
@@ -248,10 +280,21 @@ export default function Tasks() {
                     }}
                     title={t('Create new task')}
                 >
-                    <i className="bi bi-plus-circle"></i>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-plus-circle"
+                        viewBox="0 0 16 16"
+                        focusable="false"
+                    >
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                    </svg>
                     <span className="addTaskButtonSpan">
                         {' '}
-                        {t('Create new task')}
+                        {t('Create a new task')}
                     </span>
                 </button>
             </div>
