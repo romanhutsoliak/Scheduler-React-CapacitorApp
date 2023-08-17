a9c23692e3b5376d406b614f44b27542e1a661b3
Roman Hutsoliak <roman.hutsoliak@gmail.com>
2023-08-17 22:04:11 +0300

    Task view: description nl2br

--- src/components/pages/TaskView.tsx
+++ src/components/pages/TaskView.tsx
@@ -1,20 +1,12 @@
-import { useMutation, useQuery } from '@apollo/client';
-import { useNavigate, useParams } from 'react-router-dom';
+import {useMutation, useQuery} from '@apollo/client';
+import {useNavigate, useParams} from 'react-router-dom';
 import Loading from '../layoutParts/Loading';
-import { QUERY_TASK_WITH_HISTORY, COMPLETE_TASK } from '../../graphql/queries';
-import {
-    periodTypeMonthsArray,
-    periodTypeWeekDaysArray,
-    DateFormateUtils,
-    TimeToEventUtils,
-} from '../../utils';
-import BreadCrumbs, {
-    useMakePathArray,
-    updateBreadCrumbsPathArray,
-} from '../layoutParts/BreadCrumbs';
-import { useLanguage } from '../../languages';
+import {COMPLETE_TASK, QUERY_TASK_WITH_HISTORY} from '../../graphql/queries';
+import {DateFormateUtils, periodTypeMonthsArray, periodTypeWeekDaysArray, TimeToEventUtils,} from '../../utils';
+import BreadCrumbs, {updateBreadCrumbsPathArray, useMakePathArray,} from '../layoutParts/BreadCrumbs';
+import {useLanguage} from '../../languages';
 import Modal from '../layoutParts/Modal';
-import { useRef } from 'react';
+import {useRef} from 'react';
 import Nl2br from '../layoutParts/Nl2br';
 import LoadingError from '../layoutParts/LoadingError';
 import Error404 from '../layoutParts/Error404';
@@ -25,32 +17,31 @@ type TaskHistory = {
 };
 export default function TaskView() {
     const navigate = useNavigate();
-    const { taskId } = useParams();
+    const {taskId} = useParams();
     const t = useLanguage();
     const completeNotesRef = useRef<HTMLTextAreaElement>(null);
 
     const [completeTask] = useMutation(COMPLETE_TASK, {
         onError: () => null,
-        onCompleted: () => {},
+        onCompleted: () => {
+        },
         refetchQueries: ['GetTaskWithHistory'],
     });
     const task = useQuery(QUERY_TASK_WITH_HISTORY, {
-        variables: { id: taskId },
+        variables: {id: taskId},
     });
     const breadCrumbsPathArray = updateBreadCrumbsPathArray(
         1,
-        { name: task.data?.task?.name },
+        {name: task.data?.task?.name},
         useMakePathArray()
     );
 
     if (task.loading) {
-        return <Loading />;
-    }
-    else if (task.error) {
-        return <LoadingError />;
-    }
-    else if (taskId && task.data?.task === null) {
-        return <Error404 />;
+        return <Loading/>;
+    } else if (task.error) {
+        return <LoadingError/>;
+    } else if (taskId && task.data?.task === null) {
+        return <Error404/>;
     }
 
     let periodText = '';
@@ -84,7 +75,7 @@ export default function TaskView() {
     }
     return (
         <>
-            <BreadCrumbs breadCrumbsPathArray={breadCrumbsPathArray} />
+            <BreadCrumbs breadCrumbsPathArray={breadCrumbsPathArray}/>
             <Modal
                 title={t('Do you want to complete the task ?')}
                 body={
@@ -131,7 +122,8 @@ export default function TaskView() {
                                 viewBox="0 0 16 16"
                                 focusable="false"
                             >
-                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                                <path
+                                    d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                             </svg>
                         </a>
                         {task?.data?.task?.isActive ? (
@@ -151,8 +143,9 @@ export default function TaskView() {
                                     viewBox="0 0 16 16"
                                     focusable="false"
                                 >
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                    <path
+                                        d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
                                 </svg>
                             </button>
                         ) : (
@@ -165,7 +158,14 @@ export default function TaskView() {
                             {task?.data?.task?.description && (
                                 <div className="mb-3">
                                     {t('Description')}:{' '}
-                                    {task?.data?.task?.description}
+                                    {task?.data?.task?.description?.split('\n').map(function (descriptionPart: string, key: string) {
+                                        return (
+                                            <span key={key}>
+                                                {descriptionPart}
+                                                <br/>
+                                            </span>
+                                        )
+                                    })}
                                 </div>
                             )}
                             <div className="mb-3">
